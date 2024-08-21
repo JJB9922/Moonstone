fc49ea59289eff7f255899e0665903f0ba68db6e
JJB9922 <JJB9922@protonmail.com>
2024-08-21 23:00:34 +0100

    Docs

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,18 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abegiklmorvw",
-  1: "abegiklmorvw"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes"
 };
 
