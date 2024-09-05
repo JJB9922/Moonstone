38fb862f7e4a3ebb275abf744a19e84c010b3b73
JJB9922 <jjb9922@protonmail.com>
2024-09-05 18:01:36 +0200

    Fixed win32 blocker

--- Engine/src/Core/Include/Core.h
+++ Engine/src/Core/Include/Core.h
@@ -4,22 +4,41 @@
 #include "Core/Include/Logger.h"
 #include "mspch.h"
 
+#ifdef _WIN32
+    #define MS_PLATFORM_WINDOWS
+#elif defined(__linux__)
+    #define MS_PLATFORM_LINUX
+#endif
+
 #define MS_OPENGL
 //#define MS_VULKAN
 
 #define MS_ENABLE_ASSERTS
 
 #ifdef MS_ENABLE_ASSERTS
-#define MS_ASSERT(x, ...)                                                                          \
-    {                                                                                              \
-        if (!(x))                                                                                  \
-        {                                                                                          \
-            std::stringstream ss;                                                                  \
-            ss << "assert failed: " << __VA_ARGS__ << '\n';                                        \
-            MS_ERROR(ss.str());                                                                    \
-            __builtin_trap();                                                                      \
-        }                                                                                          \
-    }
+    #ifdef MS_PLATFORM_WINDOWS
+        #define MS_ASSERT(x, ...)                                                                      \
+        {                                                                                              \
+            if (!(x))                                                                                  \
+            {                                                                                          \
+                std::stringstream ss;                                                                  \
+                ss << "assert failed: " << __VA_ARGS__ << '\n';                                        \
+                MS_ERROR(ss.str());                                                                    \
+                __debugbreak();                                                                        \
+            }                                                                                          \
+        }
+    #elif defined(MS_PLATFORM_LINUX)
+        #define MS_ASSERT(x, ...)                                                                      \
+        {                                                                                              \
+            if (!(x))                                                                                  \
+            {                                                                                          \
+                std::stringstream ss;                                                                  \
+                ss << "assert failed: " << __VA_ARGS__ << '\n';                                        \
+                MS_ERROR(ss.str());                                                                    \
+                __builtin_trap();                                                                      \
+            }                                                                                          \
+        }
+    #endif
 #endif
 
 #endif // CORE_H
